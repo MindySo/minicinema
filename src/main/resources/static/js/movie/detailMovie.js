972d5feae36771363362ef5e3b09bc079ab78e2d
MindySo <mindyso7151@gmail.com>
2024-09-22 15:40:37 +0900

    detailMovie_영화추천_owlcarousel_추가

--- src/main/resources/static/js/movie/detailMovie.js
+++ src/main/resources/static/js/movie/detailMovie.js
@@ -146,4 +146,31 @@ $(document).ready(function () {
             }
         });
     });
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+    owl.on('mousewheel', '.owl-stage', function (e) {
+        if (e.deltaY>0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
+    });
 });
