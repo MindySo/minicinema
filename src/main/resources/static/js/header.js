eb5da53f8c263d0b4a77cf64ce6b4866c9768a2e
MindySo <mindyso7151@gmail.com>
2025-04-13 01:20:18 +0900

    logout_redirection_fixed

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -16,7 +16,7 @@ function logout() {
         })
         .then(response => {
             if (response.ok) {
-                window.location.replace(window.location.origin + "/");
+                window.location.replace("https://" + window.location.host + "/");
             } else {
                 alert('잠시 후 다시 시도해주세요.');
             }
